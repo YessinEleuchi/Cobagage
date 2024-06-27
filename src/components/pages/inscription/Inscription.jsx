3dfae8e438e7c83d821d9154131e363f8d9dd406
YessinEleuchi <yessineleuchi2001@gmail.com>
2024-06-27 16:31:31 +0100

    Added new feature to the project

--- src/components/pages/inscription/Inscription.jsx
+++ src/components/pages/inscription/Inscription.jsx
@@ -1,13 +1,64 @@
-import React from 'react';
-import { Container, TextField, Button, Box, Typography, Select, MenuItem, InputLabel, FormControl } from '@mui/material';
+import React, { useState } from 'react';
+import {
+    Container,
+    TextField,
+    Button,
+    Box,
+    Typography,
+    Select,
+    MenuItem,
+    InputLabel,
+    FormControl,
+    FormControlLabel,
+    Radio ,
+    RadioGroup ,
+    FormLabel ,
+    FormGroup  ,
+    Checkbox ,
+
+    
+} from '@mui/material';
+
 
 const Inscription = () => {
-    const handleSubmit = (e) =>{
+    const [genre, setGenre] = useState('');
+    const [nom,setNom] = useState('');
+    const [prenom,setPrenom] = useState('');
+    const [email,setEmail] = useState('');
+    const [password,setPassword] = useState('');
+    const [ville,setVille] = useState('');
+    const handleGenreChange = (event) => {
+        setGenre(event.target.value);
+    };
+    const handleVilleChange = (event) => {
+        setVille(event.target.value);
+    };
+    const [loisir, setLoisir] = useState({
+        music: false,
+        sport: false,
+        cinema: false,
+        lecture: false,
+    });
+    const handleLoisir = (e) => {
+        setLoisir({
+            ...loisir,
+            [e.target.name]: e.target.checked,
+        });
+    };
+
+    const handleSubmit = (e) => {
         e.preventDefault();
-    }
+        
+        alert(`${nom} , ${prenom},${email},${password},${genre},${ville} ,Loisirs: ${getSelectedLoisirs()} `);
+    };
+    const getSelectedLoisirs = () => {
+        return Object.keys(loisir)
+            .filter(key => loisir[key])
+            .join(', ');
+    };
     return (
         <Container component="main" maxWidth="xs">
-            <Box 
+            <Box
                 sx={{
                     marginTop: 8,
                     display: 'flex',
@@ -28,6 +79,7 @@ const Inscription = () => {
                         name="name"
                         autoComplete="name"
                         autoFocus
+                        onChange={(e)=>setNom(e.target.value)}
                     />
                     <TextField
                         margin="normal"
@@ -37,6 +89,8 @@ const Inscription = () => {
                         label="Prénom"
                         name="prenom"
                         autoComplete="prenom"
+                        onChange={(e) => setPrenom(e.target.value)}
+
                     />
                     <TextField
                         margin="normal"
@@ -47,6 +101,8 @@ const Inscription = () => {
                         name="email"
                         autoComplete="email"
                         type="email"
+                        onChange={(e) => setEmail(e.target.value)}
+
                     />
                     <TextField
                         margin="normal"
@@ -57,21 +113,77 @@ const Inscription = () => {
                         type="password"
                         id="password"
                         autoComplete="current-password"
+                        onChange={(e) => setPassword(e.target.value)}
+
                     />
+                    <FormControl component="fieldset" margin="normal">
+                        <RadioGroup row value={genre} onChange={handleGenreChange} >
+                        
+                            <FormControlLabel value="female" control={<Radio />} label="Femme" />
+                            <FormControlLabel value="male" control={<Radio />} label="Homme" />
+                        </RadioGroup>
+                    </FormControl>
                     <FormControl fullWidth margin="normal">
-                        <InputLabel id="type-label">Type</InputLabel>
+                        <InputLabel id="ville-label">Ville</InputLabel>
                         <Select
-                            labelId="type-label"
-                            id="type"
-                            label="Type"
+                            labelId="ville-label"
+                            id="ville"
+                            label="Ville"
                             defaultValue=""
+                            onChange={handleVilleChange}
+
                         >
-                            <MenuItem value="">
-                                <em>None</em>
-                            </MenuItem>
-                            <MenuItem value="Entreprise">Entreprise</MenuItem>
+                            <MenuItem value="Sfax">Sfax</MenuItem>
+                            <MenuItem value="Tunis">Tunis</MenuItem>
                         </Select>
                     </FormControl>
+                    <FormLabel component="legend">Loisir</FormLabel>
+                    <FormGroup>
+                        <FormControlLabel
+                            control={
+                                <Checkbox
+                                    checked={loisir.music}
+                                    onChange={handleLoisir}
+                                    name="music"
+                                    color="primary"
+                                />
+                            }
+                            label="Music"
+                        />
+                        <FormControlLabel
+                            control={
+                                <Checkbox
+                                    checked={loisir.sport}
+                                    onChange={handleLoisir}
+                                    name="sport"
+                                    color="primary"
+                                />
+                            }
+                            label="Sport"
+                        />
+                        <FormControlLabel
+                            control={
+                                <Checkbox
+                                    checked={loisir.cinema}
+                                    onChange={handleLoisir}
+                                    name="cinema"
+                                    color="primary"
+                                />
+                            }
+                            label="Cinema"
+                        />
+                        <FormControlLabel
+                            control={
+                                <Checkbox
+                                    checked={loisir.lecture}
+                                    onChange={handleLoisir}
+                                    name="lecture"
+                                    color="primary"
+                                />
+                            }
+                            label="Lecture"
+                        />
+                    </FormGroup>
                     <Button
                         type="submit"
                         fullWidth
